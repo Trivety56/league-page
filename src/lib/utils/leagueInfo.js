556f326c9913cc597f98af32d6943db8aeab7c7a
Trivety56 <121147547+Trivety56@users.noreply.github.com>
2024-09-25 19:27:13 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
-export const leagueID = "919699658312663040"; // your league ID
-export const leagueName = "The PSK Dynasty League"; // your league name
+export const leagueID = "1055735608787734528"; // your league ID
+export const leagueName = "PHI SIG DYNASTY"; // your league name
 export const dues = 25; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
